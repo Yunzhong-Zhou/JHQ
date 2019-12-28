5fdd0cbde8cd2ecba806a7b47e241297ddc5ffef
YunZhong <1125213018@qq.com>
2019-12-28 23:16:29 +0800

    优化页面

--- app/src/main/java/com/ofc/ofc/activity/AddressActivity.java
+++ app/src/main/java/com/ofc/ofc/activity/AddressActivity.java
@@ -3,15 +3,28 @@ package com.ofc.ofc.activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ofc.ofc.R;
 import com.ofc.ofc.base.BaseActivity;
+import com.ofc.ofc.utils.MyLogger;
 import com.ofc.ofc.utils.ZxingUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by zyz on 2019-12-27.
@@ -21,6 +34,34 @@ public class AddressActivity extends BaseActivity {
     ImageView imageView_addr,imageView_fuzhi;
     TextView textView_addr;
 
+    Handler handler = new Handler();
+    private static final int MSG_SUCCESS = 0;// 获取成功的标识
+    private Handler mHandler = new Handler() {
+
+        public void handleMessage(Message msg) {// 此方法在ui线程运行
+            switch (msg.what) {
+                case MSG_SUCCESS:
+                    showToast(getString(R.string.zxing_h21));
+//                    textView.setClickable(true);
+                    /*if (file != null) {
+                        showToast("二维码已经保存到相册");
+                        textView.setClickable(true);
+
+                    } else {
+                        showToast("图片保存失败", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                textView.setClickable(true);
+                                dialog.dismiss();
+                                initData();
+                            }
+                        });
+                    }*/
+                    break;
+
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,10 +94,92 @@ public class AddressActivity extends BaseActivity {
                 myToast(getString(R.string.recharge_h34));
             }
         });
+        imageView_addr.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        printScreen(imageView_addr, "OFC_qrcode" + System.currentTimeMillis());
+                    }
+                });
+                return true;
+            }
+        });
     }
 
     @Override
     protected void updateView() {
         titleView.setTitle(R.string.address_h17);
+    }
+
+    /**
+     * 截取图片存到本地
+     */
+    public File printScreen(View view, String picName) {
+        //图片地址
+        //        String imgPath = FileUtil.getImageDownloadDir(MyPosterActivity.this) + picName + ".png";
+//        String imgPath = Environment.getExternalStorageDirectory() + "/" + picName + ".png";//文件根目录
+        String imgPath = Environment.getExternalStorageDirectory()
+                + File.separator + Environment.DIRECTORY_DCIM
+                + File.separator + "Camera" + File.separator;//相册
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+        File file = null;
+        if (bitmap != null) {
+            try {
+                file = new File(imgPath, picName + ".png");
+                if (!file.exists()) {
+                    file.getParentFile().mkdirs();
+                    file.createNewFile();
+                }
+                FileOutputStream out = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+
+                /*//通知相册更新
+                MediaStore.Images.Media.insertImage(getContentResolver(), BitmapFactory.decodeFile(f.getAbsolutePath()), f.getName(), null);
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//                Uri uri1 = Uri.fromFile(f);
+                Uri uri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", f);
+                intent.setData(uri);
+                sendBroadcast(intent);*/
+
+                /*//把文件插入到系统图库
+                try {
+                    MediaStore.Images.Media.insertImage(this.getContentResolver(), f.getAbsolutePath(), f.getName(), null);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }*/
+                // 通知图库更新
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    MediaScannerConnection.scanFile(this, new String[]{file.getAbsolutePath()}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                                    mediaScanIntent.setData(uri);
+                                    sendBroadcast(mediaScanIntent);
+                                }
+                            });
+                } else {
+                    String relationDir = file.getParent();
+                    File file1 = new File(relationDir);
+                    sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.fromFile(file1.getAbsoluteFile())));
+                }
+                mHandler.obtainMessage(MSG_SUCCESS)// 获取信息
+                        .sendToTarget(); //发送信息
+
+                MyLogger.i(">>>>>>" + file);
+                return file;
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
